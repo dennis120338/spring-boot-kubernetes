6951ade6cfcdca781b5ce77c877f72d22ae019e2
dennis12038 <332297265@qq.com>
2020-05-16 21:08:51 +0800

    11

---
+++ src/main/java/com/laohand/App.java
@@ -0,0 +1,14 @@
+package com.laohand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dennis
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/laohand/controller/TestControlelr.java
@@ -0,0 +1,15 @@
+package com.laohand.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dennis
+ */
+@RestController("/")
+public class TestControlelr {
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
