967cc1fdb24d1c8fcd4bc8d36f7df31386891ff8
dennis12038 <332297265@qq.com>
2020-05-16 22:57:26 +0800

    11

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ docker build . -t spring-boot-kubernetes:1.0
 # Start docker
 # docker run -p 8089:8080  -d spring-boot-kubernetes:1.0
 # kubectl run spring-boot-kubernetes --image=spring-boot-kubernetes:1.0 --replicas=2 --port=8085 --imagePullPolicy=Never
+# kubectl expose po spring-boot-kubernetes --port=8081 --target-port=8080
--- src/main/java/com/laohand/controller/TestControlelr.java
+++ src/main/java/com/laohand/controller/TestControlelr.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestControlelr {
     @RequestMapping("/test")
     public String test() {
-        return "test";
+        return "test222";
     }
 }
