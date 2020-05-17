a5793d89417eb7c5bbc6b68567e1e6fccaff64a2
dennis12038 <332297265@qq.com>
2020-05-17 09:17:57 +0800

    11

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ basePath=$(cd `dirname $0`; pwd)
 cd ${basePath}
 git pull
 mvn -Dmaven.repo.local=/data0/temp/ clean package  -Dmaven.javadoc.skip=true
-docker build . -t spring-boot-kubernetes:1.0
+#docker build . -t spring-boot-kubernetes:1.0
+docker build . -t swr.cn-north-1.myhuaweicloud.com/dennis120338/spring-boot-kubernetes:latest
+docker push swr.cn-north-1.myhuaweicloud.com/dennis120338/spring-boot-kubernetes:latest
 # Start docker
 # docker run -p 8089:8080  -d spring-boot-kubernetes:1.0
 # kubectl run spring-boot-kubernetes --image=spring-boot-kubernetes:1.0 --replicas=2 --port=8085 --imagePullPolicy=Never
